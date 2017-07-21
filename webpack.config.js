1d622226d5894760d10abe3cb006fb8b1cb30484
ahui666 <772084015@qq.com>
2017-07-21 10:09:39 +0800

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path=require('path')
+module.exports={
+    entry:'./app.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                use:'babel-loader',
+                exclude:/node_modules/
+            }
+        ]
+    }
+}
